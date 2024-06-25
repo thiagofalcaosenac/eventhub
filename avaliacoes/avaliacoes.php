bcb85cfd9d8ce0c72c66ee15ad204708fdc1554e
thiagofalcaosenac <thiago.falcao@alunos.sc.senac.br>
2024-06-25 00:01:54 -0300

    teste

--- avaliacoes/avaliacoes.php
+++ avaliacoes/avaliacoes.php
@@ -85,7 +85,7 @@ try {
       <a href="../index.php" class="logo d-flex align-items-center me-auto">
         <!-- Uncomment the line below if you also wish to use an image logo -->
         <!-- <img src="../assets/img/logo.png" alt=""> -->
-        <h1 class="sitename">ads</h1>
+        <h1 class="sitename">EventHub</h1>
       </a>
 
       <nav id="navmenu" class="navmenu">
